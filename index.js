2ef5cc0f01fb435905627c4f4992e6a9d735db1a
Riceball LEE <snowyu.lee@gmail.com>
2015-01-16 10:30:19 +0800

    * add StreamNoSQL to index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var ReadStream = require("./lib/read-stream")
+var ReadStream  = require("./lib/read-stream")
 var WriteStream = require("./lib/write-stream")
+var StreamNoSQL = require("./lib/nosql-stream")
 
-module.exports.ReadStream   = ReadStream
-module.exports.WriteStream  = WriteStream
+StreamNoSQL.ReadStream   = ReadStream
+StreamNoSQL.WriteStream  = WriteStream
+
+module.exports  = StreamNoSQL
+
